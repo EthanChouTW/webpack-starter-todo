54511054e71d15b0dc00dfbd68d2073d99b270c1
Ethan <ethanchoudev@gmail.com>
2016-10-28 00:20:27 +0800

    delete li, css

--- entry.js
+++ entry.js
@@ -1,15 +1,22 @@
+require('./lib/css/style.css');
 import $ from 'jquery';
-import getForm from "./lib/form.data.js";
-import insertData from "./lib/insertData.js";
+import getForm from "./lib/js/form.data.js";
+import insertData from "./lib/js/insertData.js";
+import deleteData from "./lib/js/deleteData.js";
 
 let form = $("form");
 
 form.on('submit', function (e) {
 	e.preventDefault();
 	var value = getForm(e);
-	insertData(value);
-	alert(`Add item: ${value}`);
+  console.log(value);
+  if (value.length > 0) {
+    insertData(value);
+  }
+
 });
 
-
-
+let ul = $('ul');
+ul.on('click','li', (event) => {
+  deleteData(event.target);
+})
